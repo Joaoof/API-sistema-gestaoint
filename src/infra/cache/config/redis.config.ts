64d9e0c4aa54440ceb0a62e25f1ab41163d17675
Joaoof <joaodeus400@gmail.com>
2025-08-22 11:55:48 -0300

    feat: add redis config

--- src/infra/cache/config/redis.config.ts
+++ src/infra/cache/config/redis.config.ts
@@ -3,6 +3,8 @@ import Redis from 'ioredis';
 const redis = new Redis({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD
 });
 
 redis.on('error', (err) => {
