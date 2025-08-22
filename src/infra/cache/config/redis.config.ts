cbef3d3712bed816e12e93571c4a709afe73984c
JK - Sistemas <jksystem.app@gmail.com>
2025-08-22 19:22:36 -0300

    feat: redis

--- src/infra/cache/config/redis.config.ts
+++ src/infra/cache/config/redis.config.ts
@@ -1,20 +1,6 @@
 import Redis from 'ioredis';
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-    password: process.env.REDIS_PASSWORD,
-    maxRetriesPerRequest: null,
-    enableReadyCheck: false,
-    retryStrategy(times) {
-        return Math.min(times * 50, 30000);
-    },
-    // 🔽 Adicione isto:
-    keepAlive: 10000, // Envia pacote TCP a cada 10s para manter conexão
-    connectTimeout: 10000, // Timeout de conexão
-    // Opcional: se usar TLS
-    // tls: process.env.REDIS_TLS === 'true' ? {} : undefined,
-});
+const redis = new Redis(process.env.REDIS_HOST ?? '');
 
 redis.on('error', (err) => {
     console.error('🔴 Redis Connection Error:', err);
