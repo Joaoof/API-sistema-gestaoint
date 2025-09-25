a4dbe43d8366e9f445ac7dea0ab9b059940a8b51
Joaoof <joaodeus400@gmail.com>
2025-09-25 13:52:14 -0300

    feat: add debugs log

--- src/infra/database/implementations/cashMovement/cash-movement.prisma.repository.ts
+++ src/infra/database/implementations/cashMovement/cash-movement.prisma.repository.ts
@@ -50,6 +50,19 @@ export class PrismaCashMovementRepository implements CashMovementRepository {
     async findAll(userId: string): Promise<CashMovement[]> {
         console.debug('findAll userId recebido:', userId);
 
+        const cacheKey = `cashMovements:${userId}:all`;
+        const cached = await this.redis.get(cacheKey);
+        if (cached) {
+            try {
+                const arr = JSON.parse(cached);
+                if (Array.isArray(arr)) {
+                    return arr.map(CashMovement.fromPrisma);
+                }
+            } catch {
+                await this.redis.delete(cacheKey);
+            }
+        }
+
         const allRows = await this.prisma.mvCashMovementsPerUser.findMany();
         console.debug('DEBUG allRows na view (sem filtro):', allRows);
 
@@ -59,6 +72,7 @@ export class PrismaCashMovementRepository implements CashMovementRepository {
             orderBy: { date: 'desc' },
             select: {
                 id: true,
+                user_id: true,
                 type: true,
                 category: true,
                 value: true,
@@ -68,7 +82,11 @@ export class PrismaCashMovementRepository implements CashMovementRepository {
         });
 
         console.debug(`DEBUG rows filtrados por ${userId}:`, rows);
+
+
         // 2) Cache rápido
+        await this.redis.setWithPipeline(cacheKey, rows, 3600);
+
         return rows.map(CashMovement.fromPrisma);
     }
 
