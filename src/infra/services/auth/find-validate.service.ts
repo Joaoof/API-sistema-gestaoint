3847cb008a624e4f57af0ec2b895d90ea1ad2be9
Joaoof <joaodeus400@gmail.com>
2025-09-25 11:07:29 -0300

    fix: update findvalidate user

--- src/infra/services/auth/find-validate.service.ts
+++ src/infra/services/auth/find-validate.service.ts
@@ -1,49 +1,28 @@
-import { PrismaService } from "prisma/prisma.service";
-import { InvalidCredentialsError } from "src/core/exceptions/invalid-credentials.exception";
-import { ValidatePassword } from "./validate-password.service";
-import { User } from "src/core/entities/user.entity";
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { ValidatePassword } from './validate-password.service';
+import { InvalidCredentialsError } from 'src/core/exceptions/invalid-credentials.exception';
 
 @Injectable()
 export class FindValidateUser {
     constructor(
         private readonly prisma: PrismaService,
-        private readonly validatePassword: ValidatePassword
+        private readonly validatePassword: ValidatePassword,
     ) { }
 
-    async isValid(email: string, passwordInput: string): Promise<User> {
-        const userRecord = await this.prisma.users.findUnique({
+    async isValid(email: string, passwordInput: string) {
+        const record = await this.prisma.users.findUnique({
             where: { email },
-            select: {
-                id: true,
-                email: true,
-                password_hash: true,
-                role: true,
-                company_id: true,
-            }
+            select: { id: true, email: true, password_hash: true },
         });
+        if (!record) throw new InvalidCredentialsError();
 
-        if (!userRecord) {
-            throw new InvalidCredentialsError();
-        }
-
-        // 2️⃣ Validar senha
-        const isValid = await this.validatePassword.isValid(
-            userRecord.password_hash,
-            passwordInput
+        const valid = await this.validatePassword.isValid(
+            record.password_hash,
+            passwordInput,
         );
-        if (!isValid) {
-            throw new InvalidCredentialsError();
-        }
+        if (!valid) throw new InvalidCredentialsError();
 
-        return {
-            id: userRecord.id,
-            email: userRecord.email,
-            password_hash: userRecord.password_hash,
-            role: userRecord.role,
-            company_id: userRecord.company_id,
-            is_active: true,     // ou conforme entidade
-            createdAt: null      // preencher se necessário
-        } as User;
+        return record;
     }
 }
