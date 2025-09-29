70bf4178fc6ab23a1ca30e96ff869ef17a250b81
Joaoof <joaodeus400@gmail.com>
2025-09-29 11:02:56 -0300

    feat: add validation userId

--- src/core/use-cases/cashMovement/create-cash-movement.use-case.ts
+++ src/core/use-cases/cashMovement/create-cash-movement.use-case.ts
@@ -1,8 +1,8 @@
-import { Inject } from '@nestjs/common';
+import { CashMovementMapper } from '@modules/cashMovement/mappers/entry-movement.mapper';
+import { BadRequestException, Inject } from '@nestjs/common';
 import { CashMovement } from 'src/core/entities/movements/cash-movement.entity';
 import { CashMovementRepository } from 'src/core/ports/cash-movement.repository';
 import { CreateCashMovementDto, CreateCashMovementSchema } from 'src/modules/cashMovement/dtos/cash-movement.dto';
-import { CashMovementMapper } from 'src/modules/cashMovement/mappers/entry-movement.mapper';
 
 export class CreateCashMovementUseCase {
     constructor(
@@ -11,7 +11,15 @@ export class CreateCashMovementUseCase {
     ) { }
 
     async execute(dto: CreateCashMovementDto, userId: string): Promise<CashMovement> {
-        const validatedDto = CreateCashMovementSchema.parse(dto);
+
+        if (!userId) {
+            throw new BadRequestException('userId é obrigatório');
+        }
+
+        const validatedDto = CreateCashMovementSchema.parse({
+            ...dto,
+            userId: userId
+        });
 
         const movement = CashMovementMapper.toDomain(validatedDto, userId);
         await this.cashMovementRepo.create(movement);
