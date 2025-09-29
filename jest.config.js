b6323e2a9a489a65797262fe066f1a3bbd421900
Joaoof <joaodeus400@gmail.com>
2025-09-29 11:01:09 -0300

    chore: add jest config js

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: 'src',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',  // pega só unit tests
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+    '^@modules/(.*)$': '<rootDir>/modules/$1',
+    '^@core/(.*)$': '<rootDir>/core/$1',
+  },
+};
