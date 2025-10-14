4bff3ca9de705c00ada86d6ce82cc4eb56abeb4c
Joaoof <joaodeus400@gmail.com>
2025-10-14 14:32:39 -0300

    refactor: comment findy-user-by-id

--- src/core/use-cases/users/create-users/find-user-by-id.use-case.ts
+++ src/core/use-cases/users/create-users/find-user-by-id.use-case.ts
@@ -1,21 +1,21 @@
-import { Inject } from '@nestjs/common';
-import { User } from 'src/core/entities/user.entity';
-import { NotFoundError } from 'src/core/exceptions/api.exception';
-import { UserRepository } from 'src/core/ports/user.repository';
+// import { Inject } from '@nestjs/common';
+// import { User } from 'src/core/entities/user.entity';
+// import { NotFoundError } from 'src/core/exceptions/api.exception';
+// import { UserRepository } from 'src/core/ports/user.repository';
 
-export class FindUserByIdUseCase {
-  private readonly userRepository: UserRepository;
-  constructor(@Inject('UsersRepository') userRepository: UserRepository) {
-    this.userRepository = userRepository;
-  }
+// export class FindUserByIdUseCase {
+//   private readonly userRepository: UserRepository;
+//   constructor(@Inject('UsersRepository') userRepository: UserRepository) {
+//     this.userRepository = userRepository;
+//   }
 
-  async execute(id: string): Promise<User> {
-    const user = await this.userRepository.findById(id);
+//   async execute(id: string): Promise<User> {
+//     const user = await this.userRepository.findById(id);
 
-    if (!user) {
-      throw new NotFoundError(`User not found`, id);
-    }
+//     if (!user) {
+//       throw new NotFoundError(`User not found`, id);
+//     }
 
-    return user;
-  }
-}
+//     return user;
+//   }
+// }
