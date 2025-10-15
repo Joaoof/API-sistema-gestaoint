c868800379c99cb7306251f46686aa1509d3e4e8
Joaoof <joaodeus400@gmail.com>
2025-10-15 13:58:31 -0300

    feat: add update module

--- src/modules/cashMovement/cash-movement.module.ts
+++ src/modules/cashMovement/cash-movement.module.ts
@@ -7,6 +7,7 @@ import { RedisModule } from '../../infra/cache/redis.module';
 import { PrismaCashMovementRepository } from '../../infra/database/implementations/cashMovement/cash-movement.prisma.repository';
 import { CashMovementResolver } from '../../infra/graphql/resolvers/movements/cash-movement.resolver';
 import { DeleteCashMovementUseCase } from '../../core/use-cases/cashMovement/delete-cash-movement.use-case';
+import { UpdateCashMovementUseCase } from 'src/core/use-cases/cashMovement/update-cash-movement.use-case';
 
 @Module({
   imports: [PrismaModule, RedisModule],
@@ -16,6 +17,7 @@ import { DeleteCashMovementUseCase } from '../../core/use-cases/cashMovement/del
     FindAllCashMovementUseCase,
     DashboardMovementUseCase,
     DeleteCashMovementUseCase,
+    UpdateCashMovementUseCase,
     {
       provide: 'CashMovementRepository',
       useClass: PrismaCashMovementRepository,
