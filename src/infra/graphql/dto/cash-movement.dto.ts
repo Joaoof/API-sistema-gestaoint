f840caa68dd47b85110b332bcdc95412996d9ab2
Joaoof <joaodeus400@gmail.com>
2025-11-07 14:34:44 -0300

    fix: remove space line cashMovementypePayment

--- src/infra/graphql/dto/cash-movement.dto.ts
+++ src/infra/graphql/dto/cash-movement.dto.ts
@@ -4,10 +4,7 @@ import {
   CashMovementTypes,
 } from '../enum/cash-movement-type.enum';
 import { CashMovementCategories, CashMovementCategory } from '../enum/cash-movement-category.enum';
-import {
-  MovementTypePayments,
-  MovementTypePayment,
-} from '../enum/cash-movement-type-payment.enum';
+import { MovementTypePayments, MovementTypePayment } from '../enum/cash-movement-type-payment.enum';
 
 @ObjectType()
 export class CashMovementGraphQL {
